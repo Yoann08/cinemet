2a71f105cb86cff9aaaccea2c3b26fc64caa5676
Yoann <yoannlaporte08@gmail.com>
2023-03-16 11:34:48 +0100

    creation nav et footer

---
+++ content/navbar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+
+<nav>
+    <ul>
+        <li>Accueil</li>
+        <li>A l'affiche</li>
+        <li>contact</li>
+    </ul>
+</nav>
+
+</body>
+</html>
