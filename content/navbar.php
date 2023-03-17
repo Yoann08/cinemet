4b69aa31dd48acbc39b6fecff356d67902089322
Yoann <yoannlaporte08@gmail.com>
2023-03-17 16:20:48 +0100

    page d'accueil a finir

--- content/navbar.php
+++ content/navbar.php
@@ -1,21 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
-</head>
-<body>
-
-<nav>
-    <ul>
-        <li>Accueil</li>
-        <li>A l'affiche</li>
-        <li>contact</li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="./asset/img/Logo.png" alt="Logo Cinemet">
+            </a>
+            <button class="navbar-toggler" type="button"
+            data-bs-toggle="collapse" data-bs-target="#menu" 
+            aria-controls="navbarSupportedContent" aria-expanded="false" 
+            aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link font-text">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link font-text">A L'Affiche</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link font-text">Contact</a>
+                    </li>
+                </ul>
+                <div class="ms-auto d-flex align-items-center">
+                    <button type="button" class="btn btn-dark">
+                        <i class="fa-regular fa-user"></i>
+                        <span class="font-text">Connexion</span>
+                    </button>
+                </div>
+            </div>
+        </div>
 </nav>
-
-</body>
-</html>
